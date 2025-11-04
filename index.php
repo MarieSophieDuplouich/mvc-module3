b0b16ed75bdc1c33612fd2551fefd8f6fd63263d
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-11-04 09:46:04 +0100

    installation du projet

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<!-- Créer la structure des dossiers du projet
+Créer le fichier index.php
+Mettre en place la réécriture d'URL pour que index.php reçoivent toutes les URLs à l'aide du fichier .htaccess
+Créer la classe App qui sera le point de départ de notre application. -->
